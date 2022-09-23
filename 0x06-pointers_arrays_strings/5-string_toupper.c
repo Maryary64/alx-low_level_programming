c112950e771018fa059171d79f2696dfe484ec46
Mary Oloyede <maryoloyedeayo@gmail.com>
2022-09-22 17:44:05 -0700

    fifth

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+* *string_toupper - changes all lower case letter
+*       of a string to uppercase.
+* @s: source string.
+* Return: an uppercase string
+*/
+
+char *string_toupper(char *s)
+{
+	int i = 0;
+
+	while (s[i])
+	{
+		s[i] = toupper(s[i]);
+
+		i++;
+	}
+
+	return (s);
+}
