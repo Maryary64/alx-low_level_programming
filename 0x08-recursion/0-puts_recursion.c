09b73caefad73c98e65610189ff38531f6bfc583
Mary Oloyede <maryoloyedeayo@gmail.com>
2022-09-28 08:53:14 -0700

    tough betty

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,12 +6,12 @@
 */
 void _puts_recursion(char *s)
 {
-	if (*s == '\0')
-	{
-		putchar('\n');
-		return;
-	}
-	_putchar(*s);
-	s++;
-	_puts_recursion(s);
+        if (*s == '\0')
+        {
+                putchar('\n');
+                return;
+        }
+	putchar(*s);
+        s++;
+        _puts_recursion(s);
 }
