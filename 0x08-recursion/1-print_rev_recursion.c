2f637ccb150873373ba7c31b697504bd306f073f
Mary Oloyede <maryoloyedeayo@gmail.com>
2022-09-29 13:30:06 -0700

    hot fix

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
- * _print_rev_recursion - prints in reverse recurse
- * @s: string to recurse
+ * _print_rev_recursion - Prints a string in reverse.
+ * @s: The string to be printed.
  */
-
 void _print_rev_recursion(char *s)
 {
 	if (*s)
